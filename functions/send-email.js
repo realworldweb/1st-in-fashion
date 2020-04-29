31d1454707a9d4ff80c23cc5e40898d4d917e2c4
paul <paulrooney60@gmail.com>
2020-04-30 00:20:35 +0100

    email.js update

--- functions/send-email.js
+++ functions/send-email.js
@@ -1,15 +1,27 @@
 const SparkPost = require('sparkpost');
-const client = new SparkPost(process.env.sparkPost);
+const client = new SparkPost(process.env.SPARKPOST);
 
-exports.handler = function(event, context, callback) {
-  client.transmissions
-    .send({
-      content: {
-        from: 'email.realworldweb.com',
-        subject: 'Hello, World!',
-        html:
-          "<html><body><p>My cool email.</p></body></html>"
-      },
-    recipients: [{ address: 'paulrooney60@gmail.com' }]
+// If you have a SparkPost EU account you will need to pass a different `origin` via the options parameter:
+// const euClient = new SparkPost('<YOUR API KEY>', { origin: 'https://api.eu.sparkpost.com:443' });
+
+client.transmissions.send({
+    options: {
+      sandbox: true
+    },
+    content: {
+      from: 'example@examp.com',
+      subject: 'Hello, World!',
+      html:'<html><body><p>Testing SparkPost - the world\'s most awesomest email service!</p></body></html>'
+    },
+    recipients: [
+      {address: 'paulrooney60@gmail.com'}
+    ]
+  })
+  .then(data => {
+    console.log('Woohoo! You just sent your first mailing!');
+    console.log(data);
+  })
+  .catch(err => {
+    console.log('Whoops! Something went wrong');
+    console.log(err);
   });
-}
