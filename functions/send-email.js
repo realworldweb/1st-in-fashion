c373eeb415e9807151b7fcbeb76cb8ea3e003e29
paul <paulrooney60@gmail.com>
2020-04-30 00:28:47 +0100

    minor update

--- functions/send-email.js
+++ functions/send-email.js
@@ -1,5 +1,5 @@
 const SparkPost = require('sparkpost');
-const client = new SparkPost(process.env.SPARKPOST);
+const client = new SparkPost(process.env.SPARKPOST, { origin: 'https://api.eu.sparkpost.com:443' });
 
 // If you have a SparkPost EU account you will need to pass a different `origin` via the options parameter:
 // const euClient = new SparkPost('<YOUR API KEY>', { origin: 'https://api.eu.sparkpost.com:443' });
