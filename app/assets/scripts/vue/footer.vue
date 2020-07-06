326ed8c2df85687b76b4599e0258ddff413e029f
paul <paulrooney60@gmail.com>
2020-07-06 23:11:46 +0100

    added router link for footer

--- app/assets/scripts/vue/footer.vue
+++ app/assets/scripts/vue/footer.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 <div id="footer" class="site-footer">
-<div class="wrapper"><p><span class="site-footer__text">Copyright &copy; 2019 Realworld Web. All rights reserved.</span> <a href="#" class="btn btn--white open-modal">Get in Touch</a></p></div>
+<div class="wrapper"><p><span class="site-footer__text">Copyright &copy; 2019 Realworld Web. All rights reserved.</span> <router-link tag="a" to="/info" class="btn btn--white open-modal">Get in Touch</router-link></p></div>
 </div>
 </template>
 
