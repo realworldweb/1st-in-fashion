6bd8ba97d2ccfae6a3d90f91a996d4969417fc18
paul <paulrooney60@gmail.com>
2020-08-08 12:03:54 +0100

    updated pictures to work with history mode

--- app/assets/scripts/vue/stages/new-category-stage.vue
+++ app/assets/scripts/vue/stages/new-category-stage.vue
@@ -4,7 +4,7 @@
 <p class="product-pane__title">{{$route.params.id}}</p>
 <div class="product-pane__products">
 <router-link tag="a" class="product-tile" :key="product.id" v-for="(product, index) in item.products" v-if="index < pagelimit && index >= currentitem" :to="'/product/'+product.id">
-<img class="product-tile__img lazyload" :src="'./assets/images'+product.img.url">
+<img class="product-tile__img lazyload" :src="'/./assets/images'+product.img.url">
 <p class="product-tile__price">&#163;{{product.price}}</p>
 </router-link>
 </div>
