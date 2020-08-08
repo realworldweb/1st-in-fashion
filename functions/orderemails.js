fe40f2b1d0d8f273a2ea122de4d0d9821f9413d5
paul <paulrooney60@gmail.com>
2020-08-08 21:23:12 +0100

    styled order email

--- functions/orderemails.js
+++ functions/orderemails.js
@@ -21,7 +21,32 @@ if (event.body) {
     content: {
       from: 'sales@1stinfashion.co.uk',
       subject: 'sales request',
-      html:'<html><body><p>Order for<br>items:<img src="'+body.img+'"><br>'+''+body.item+''+'</p><p>sendingaddress:<br>'+''+body.address+''+'</p></body></html>'
+      html:`<html><head><style>img{ position: relative; display: block; margin: 0 auto 0 auto; } .confirmed__items{ position: relative;
+		width: 60%;
+		display: block;
+		margin: 0 auto 0 auto;
+		border: 2px solid $mainBlue;
+		font-size: 1.5rem;
+	th:first-child{
+    width: 80%;
+     }		 
+		th{
+			
+			background-color: $mainBlue;
+			color: #fff;
+			width: 20%;
+		}
+		tr{
+		 text-align: center;
+		 border: 2px solid $mainBlue;
+		 }
+		td{
+		border: 2px solid $mainBlue;
+			}}</style>
+			</head>
+			<body>
+			<p>Order for<br>items:<img class="img" src="'+body.img+'"><br>'+''+body.item+''+'</p>
+			<p>sendingaddress:<br>'+''+body.address+''+'</p></body></html>`
     },
     recipients: [
       {address: 'pr.1stinfashion@gmail.com'}
