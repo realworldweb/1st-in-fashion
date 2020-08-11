9f52b125ed052698f0c09d9d657d0cf72ef1fd5a
paul <paulrooney60@gmail.com>
2020-08-12 00:48:41 +0100

    minor update

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -99,15 +99,15 @@ new Vue({
   components: { 'sitenav': Sitenav, 'stage': Stage, 'sitefoot': Sitefoot},
   data:{ categories: null, products: null, subcategories: null, basketcontents: [], baskettotal: null, paypalitems: [], applyhover: []},
   created: function (){
-	  Axios.get('./vue/vue-data/categories.js').then( response => {
+	  Axios.get('/./vue/vue-data/categories.js').then( response => {
 		  
 		 this.categories = response.data
 	  })
-	  	  Axios.get('./vue/vue-data/products.js').then( response => {
+	  	  Axios.get('/./vue/vue-data/products.js').then( response => {
 			  
 		 this.products = response.data
 	  })
-	  	  Axios.get('./vue/vue-data/subcategories.js').then( response => {
+	  	  Axios.get('/./vue/vue-data/subcategories.js').then( response => {
 			
 		 this.subcategories = response.data
 		
