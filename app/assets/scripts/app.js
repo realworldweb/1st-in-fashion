74385bd0cd6b4da818d013bf6814040f40a33fbe
paul <paulrooney60@gmail.com>
2020-08-12 02:10:13 +0100

    minor update

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,6 +1,11 @@
 import '../styles/styles.css'
 import Axios from 'axios'
-const instance = Axios.create({baseURL: 'http://1stinfashion.co.uk'});
+const instance = Axios.create({ baseURL: 'https://1stinfashion.co.uk',
+ headers: {
+         
+      'Access-Control-Allow-Origin': '*'
+    }
+});
 
 
 
